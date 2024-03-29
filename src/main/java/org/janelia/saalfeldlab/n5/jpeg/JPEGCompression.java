b06735d34eb38b886a600f959bdc7aaaf57d4f87
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2024-03-29 13:59:19 -0400

    wip: don't interleave but map with min, max, gamma (not yet working)

--- src/main/java/org/janelia/saalfeldlab/n5/jpeg/JPEGCompression.java
+++ src/main/java/org/janelia/saalfeldlab/n5/jpeg/JPEGCompression.java
@@ -55,21 +55,59 @@ public class JPEGCompression implements BlockReader, BlockWriter, Compression {
 	@CompressionParameter
 	private final DataType dataType;
 
-	public JPEGCompression(final int quality, final DataType dataType) {
+	@CompressionParameter
+	private final double min;
+
+	@CompressionParameter
+	private final double max;
+	private final double range;
+
+	@CompressionParameter
+	private final double gamma;
+	private final double invGamma;
+
+
+	public JPEGCompression(
+			final int quality,
+			final DataType dataType,
+			final double min,
+			final double max,
+			final double gamma) {
 
 		this.quality = quality;
 		this.dataType = dataType;
+		this.min = min;
+		this.max = max;
+		range = max - min;
+		this.gamma = gamma;
+		invGamma = 1.0 / gamma;
 	}
 
 	public JPEGCompression(final int quality) {
 
-		this(quality, DataType.UINT8);
+		this(quality, DataType.UINT8, 0, 255, 1);
 	}
 
 	public JPEGCompression() {
 
 		this(100);
 	}
+
+	private final byte map(final double value) {
+
+		return (byte)Math.min(255, Math.max(0, Math.round(Math.pow((value - min) / range, gamma) * 255)));
+	}
+
+	private final double mapInv(final byte value) {
+
+		return Math.pow((value & 0xff) / 255.0, invGamma) * range + min;
+	}
+
+	private final static long clip(final double value, final double min, final double max) {
+
+		return Math.round(Math.min(max, Math.max(min, value)));
+	}
+
 
 	@Override
 	public <T, B extends DataBlock<T>> void read(
@@ -81,191 +119,73 @@ public class JPEGCompression implements BlockReader, BlockWriter, Compression {
 
 //		new ImagePlus("", new ByteProcessor(dataBlock.getSize()[0], bytes.length / dataBlock.getSize()[0], bytes)).show();
 
-		if (dataType == DataType.UINT8) {
-			final Object dataBlockData = dataBlock.getData();
-			if (dataBlockData instanceof byte[]) {
-				final byte[] data = (byte[])dataBlockData;
-				if (dataType == DataType.UINT8)
-					System.arraycopy(bytes, 0, data, 0, data.length);
-				return;
-			}
-		} else if (dataType == DataType.INT8) {
-			final byte[] data = (byte[])dataBlock.getData();
-			for (int i = 0; i < data.length; ++i)
-				data[i] = (byte)((bytes[i] & 0xff) - 127);
+		final Object dataBlockData = dataBlock.getData();
+
+		if (dataType == DataType.UINT8 && min == 0 && max == 255 && gamma == 1 && dataBlockData instanceof byte[]) {
+			final byte[] data = (byte[])dataBlockData;
+			System.arraycopy(bytes, 0, data, 0, data.length);
 			return;
 		}
 
-		final int[] blockSize = dataBlock.getSize();
-		final int width = blockSize[0];
-		final int height = (int)Math.ceil(dataBlock.getNumElements() / width / 8);
-		final int stride = width * height * 8;
-
 		switch (dataType) {
+		case UINT8:
+		case INT8:
+			final byte[] byteData = (byte[])dataBlockData;
+			if (dataType == DataType.UINT8)
+				for (int i = 0; i < byteData.length; ++i)
+					byteData[i] = (byte)clip(mapInv(bytes[i]), 0, 0xff);
+			else
+				for (int i = 0; i < byteData.length; ++i)
+					byteData[i] = (byte)clip(mapInv(bytes[i]), Byte.MIN_VALUE, Byte.MAX_VALUE);
+			break;
 		case UINT16:
 		case INT16:
-			final short[] shortData = (short[])dataBlock.getData();
-			interleave(bytes, shortData, stride);
-			if (dataType == DataType.INT16)
+			final short[] shortData = (short[])dataBlockData;
+			if (dataType == DataType.UINT16)
 				for (int i = 0; i < shortData.length; ++i)
-					shortData[i] = (short)((shortData[i] & 0xffff) - 32767);
+					shortData[i] = (short)clip(mapInv(bytes[i]), 0, 0xffff);
+			else
+				for (int i = 0; i < shortData.length; ++i)
+					shortData[i] = (short)clip(mapInv(bytes[i]), Short.MIN_VALUE, Short.MAX_VALUE);
 			break;
 		case UINT32:
 		case INT32:
-			final int[] intData = (int[])dataBlock.getData();
-			interleave(bytes, intData, stride);
-			if (dataType == DataType.INT32)
+			final int[] intData = (int[])dataBlockData;
+			if (dataType == DataType.UINT32)
 				for (int i = 0; i < intData.length; ++i)
-					intData[i] = (int)((intData[i] & 0xffffffffL) - 1073741823);
+					intData[i] = (int)clip(mapInv(bytes[i]), 0, 0xffffffffL);
+			else
+				for (int i = 0; i < intData.length; ++i)
+					intData[i] = (short)clip(mapInv(bytes[i]), Integer.MIN_VALUE, Integer.MAX_VALUE);
 			break;
 		case UINT64:
-			final long[] longData = (long[])dataBlock.getData();
-			interleave(bytes, longData, stride);
+		case INT64:
+			final long[] longData = (long[])dataBlockData;
+			if (dataType == DataType.UINT64)
+				for (int i = 0; i < longData.length; ++i)
+					longData[i] = (long)clip(mapInv(bytes[i]), 0, 1.8446744073709552E19);
+			else
+				for (int i = 0; i < longData.length; ++i)
+					longData[i] = (short)clip(mapInv(bytes[i]), Long.MIN_VALUE, Long.MAX_VALUE);
+			break;
+		case FLOAT32:
+			final float[] floatData = (float[])dataBlockData;
+			for (int i = 0; i < floatData.length; ++i)
+				floatData[i] = (float)mapInv(bytes[i]);
+			break;
+		case FLOAT64:
+			final double[] doubleData = (double[])dataBlockData;
+			for (int i = 0; i < doubleData.length; ++i)
+				doubleData[i] = mapInv(bytes[i]);
 			break;
 		default:
-			/* default reader that works for all types */
+			/* default reader applies no min, max, gamma mapping and just reads the decompressed bytes */
 			final ByteBuffer buffer = dataBlock.toByteBuffer();
 			buffer.put(bytes, 0, buffer.capacity());
 			dataBlock.readData(buffer);
 		}
 	}
 
-	private static void interleave(
-			final byte[] in,
-			final short[] out,
-			final int stride) {
-
-		for (int i = 0; i < out.length; ++i)
-			out[i] = (short)(
-					((in[i] & 0xff) << 8) |
-					(in[i + stride] & 0xff));
-	}
-
-	private static void interleave(
-			final byte[] in,
-			final int[] out,
-			final int stride) {
-
-		final int stride1 = stride + stride;
-		final int stride2 = stride1 + stride;
-
-		for (int i = 0; i < out.length; ++i)
-			out[i] =
-					((in[i] & 0xff) << 24) |
-					((in[i + stride] & 0xff) << 16) |
-					((in[i + stride1] & 0xff) << 8) |
-					(in[i + stride2] & 0xff);
-	}
-
-	private static void interleave(
-			final byte[] in,
-			final long[] out,
-			final int stride) {
-
-		final int stride1 = stride + stride;
-		final int stride2 = stride1 + stride;
-		final int stride3 = stride2 + stride;
-		final int stride4 = stride3 + stride;
-		final int stride5 = stride4 + stride;
-		final int stride6 = stride5 + stride;
-
-		for (int i = 0; i < out.length; ++i)
-			out[i] =
-					((in[i] & 0xffL) << 56) |
-					((in[i + stride] & 0xffL) << 48) |
-					((in[i + stride1] & 0xffL) << 40) |
-					((in[i + stride2] & 0xffL) << 32) |
-					((in[i + stride3] & 0xffL) << 24) |
-					((in[i + stride4] & 0xffL) << 16) |
-					((in[i + stride5] & 0xffL) << 8) |
-					(in[i + stride6] & 0xffL);
-	}
-
-	private static byte[] deInterleave(final short[] data, final int stride) {
-
-		final byte[] bytes = new byte[stride << 1];
-		for (int i = 0; i < data.length; ++i) {
-			bytes[i] = (byte)((data[i] >> 8) & 0xff);
-			bytes[i + stride] = (byte)(data[i] & 0xff);
-
-		}
-		return bytes;
-	}
-
-	private static byte[] deInterleave(final int[] data, final int stride) {
-
-		final int stride1 = stride + stride;
-		final int stride2 = stride1 + stride;
-
-		final byte[] bytes = new byte[stride << 2];
-		for (int i = 0; i < data.length; ++i) {
-			bytes[i] = (byte)((data[i] >> 24) & 0xff);
-			bytes[i + stride] = (byte)((data[i] >> 16) & 0xff);
-			bytes[i + stride1] = (byte)((data[i] >> 8) & 0xff);
-			bytes[i + stride2] = (byte)(data[i] & 0xff);
-		}
-		return bytes;
-	}
-
-	private static byte[] deInterleave(final long[] data, final int stride) {
-
-		final int stride1 = stride + stride;
-		final int stride2 = stride1 + stride;
-		final int stride3 = stride2 + stride;
-		final int stride4 = stride3 + stride;
-		final int stride5 = stride4 + stride;
-		final int stride6 = stride5 + stride;
-
-		final byte[] bytes = new byte[stride << 3];
-		for (int i = 0; i < data.length; ++i) {
-			bytes[i] = (byte)((data[i] >> 56) & 0xff);
-			bytes[i + stride] = (byte)((data[i] >> 48) & 0xff);
-			bytes[i + stride1] = (byte)((data[i] >> 40) & 0xff);
-			bytes[i + stride2] = (byte)((data[i] >> 32) & 0xff);
-			bytes[i + stride3] = (byte)((data[i] >> 24) & 0xff);
-			bytes[i + stride4] = (byte)((data[i] >> 16) & 0xff);
-			bytes[i + stride5] = (byte)((data[i] >> 8) & 0xff);
-			bytes[i + stride6] = (byte)(data[i] & 0xff);
-		}
-		return bytes;
-	}
-
-	private <T> byte[] shuffleBytes(
-			final DataBlock<T> dataBlock,
-			final int stride) {
-
-		if (dataType == DataType.UINT8) {
-			final Object dataBlockData = dataBlock.getData();
-			if (dataBlockData instanceof byte[])
-				return (byte[])dataBlockData;
-		}
-
-		switch (dataType) {
-		case UINT16:
-			return deInterleave((short[])dataBlock.getData(), stride);
-		case UINT32:
-			return deInterleave((int[])dataBlock.getData(), stride);
-		case UINT64:
-			return deInterleave((long[])dataBlock.getData(), stride);
-		case INT8:
-			final byte[] bytes = (byte[])dataBlock.getData();
-			for (int i = 0; i < bytes.length; ++i)
-				bytes[i] = (byte)(bytes[i] + 127);
-			return bytes;
-		case INT16:
-			final short[] shorts = (short[])dataBlock.getData();
-			for (int i = 0; i < shorts.length; ++i)
-				shorts[i] = (short)(shorts[i] + 32767);
-			return deInterleave(shorts, stride);
-		case INT32:
-			final int[] ints = (int[])dataBlock.getData();
-			for (int i = 0; i < ints.length; ++i)
-				ints[i] = ints[i] + 1073741823;
-			return deInterleave(ints, stride);
-		default:
-			return dataBlock.toByteBuffer().array();
-		}
-	}
 
 	@Override
 	public <T> void write(
@@ -274,10 +194,54 @@ public class JPEGCompression implements BlockReader, BlockWriter, Compression {
 
 		final int[] blockSize = dataBlock.getSize();
 		final int width = blockSize[0];
-		final int height = (int)Math.ceil(dataBlock.getNumElements() / width / 8);
-		final int stride = width * height * 8;
 
-		final byte[] bytes = shuffleBytes(dataBlock, stride);
+		byte[] bytes = new byte[dataBlock.getNumElements()];
+
+		switch (dataType) {
+		case UINT8:
+		case INT8:
+			final byte[] byteData = (byte[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(byteData[i]);
+			}
+		break;
+		case UINT16:
+		case INT16:
+			final short[] shortData = (short[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(shortData[i]);
+			}
+		break;
+		case UINT32:
+		case INT32:
+			final int[] intData = (int[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(intData[i]);
+			}
+		break;
+		case UINT64:
+		case INT64:
+			final long[] longData = (long[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(longData[i]);
+			}
+		break;
+		case FLOAT32:
+			final float[] floatData = (float[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(floatData[i]);
+			}
+		break;
+		case FLOAT64:
+			final double[] doubleData = (double[])dataBlock.getData();
+			for (int i = 0; i < bytes.length; ++i) {
+				bytes[i] = map(doubleData[i]);
+			}
+		break;
+		default:
+			bytes = dataBlock.toByteBuffer().array();
+		}
+
 		final BufferedImage img = new BufferedImage(
 				width,
 				(int)Math.ceil(bytes.length / width),
